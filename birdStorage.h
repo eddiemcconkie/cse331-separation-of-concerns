46d32221fec1feb889af1a24f26acd9eceeb22e7
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-06-10 07:52:17 -0600

    commented out all the things that are inherited
    most of birdStrorage stuff can already be found in the parent classes

--- birdStorage.h
+++ birdStorage.h
@@ -4,33 +4,35 @@
 #include "point.h"
 #include "object-type.h"
 
+//Everything that has been commented out is because all those things are inherited by the parent classes
+
 class BirdStorage : public CollidableObject
 {
 public:
-	Point getPosition() { return pt; }
-	void setPosition(Point pt) { this->pt = pt; }
+	//Point getPosition() { return pt; }
+	//void setPosition(Point pt) { this->pt = pt; }
 
 	Velocity getVelocity() { return v; }
 	void setVelocity(Velocity v) { this->v = v; }
 	
-	double getRadius() { return radius; }
-	void setRadius(double radius) { this->radius = radius; }
+	//double getRadius() { return radius; }
+	//void setRadius(double radius) { this->radius = radius; }
 
-	ObjectType getType() { return type; }
-	void setType(ObjectType type) { this->type = type; }
+	//ObjectType getType() { return type; }
+	//void setType(ObjectType type) { this->type = type; }
 
-	bool getDead() { return dead; }
-	void setDead(bool status) { dead = status; }
+	//bool getDead() { return dead; }
+	//void setDead(bool status) { dead = status; }
 
-	int getPoints() { return points; }
-	void setPoints(int points) { this->points = points; }
+	//int getPoints() { return points; }
+	//void setPoints(int points) { this->points = points; }
 
 private:
-	Point pt;
+	//Point pt;
 	Velocity v;
-	double radius;
-	ObjectType type;
-	bool dead;
-	int points;
+	//double radius;
+	//ObjectType type;
+	//bool dead;
+	//int points;
 };
 
